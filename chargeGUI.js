bca471721569fff8dc46c4596cab585c3989a894
Antonio Alvaro Almero <oinotnaoremla@gmail.com>
2026-04-21 01:20:08 +0800

    feat: set up express

---
+++ chargeGUI.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.port || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/charge.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on localhost:${PORT}`);
+});
